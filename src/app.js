92e9ce158a57bed84ecfe35e9ec863e48e4ac75f
sivaprasad1358 <sivaprasadtelukutla@gmail.com>
2025-06-22 19:30:28 +0530

    route-handler

--- src/app.js
+++ src/app.js
@@ -4,19 +4,17 @@ const app = express()
 //user/:userId/:name/:password => req.params
 //user => in postman user?userId=101 
 
+//mutlipule route handler 
+//"/user" => it is a rout 
+//(req,res,next) => it is a route handler 
+//if responsd is empty.the response state is pending 
 
-app.get("/user/:userId/:name/:password", (req,res)=>{
-    console.log(req.params)
-    res.send({firstName:"siva", second:"telukutla", age:"25"})
-})
-
-app.post("/user", (req,res)=>{
-    res.send("successfully the data is save")
-})
-
-app.delete("/user", (req,res)=>{
-    res.send("successfully the data is delete")
-})
+app.get("/user", [(req,res,next)=>{
+    // res.send("user data 1") => if first response is empty its go to second response
+    next()
+},(req,res, next)=>{
+    res.send("user data 2") //in the last response don't use next() function its give error
+}])
 
 app.listen(3000, ()=>{
     console.log("server is running")
