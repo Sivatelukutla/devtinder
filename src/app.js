becefdc2d04aa3c8ad041c01f9d82edc1efeef9e
sivaprasad1358 <sivaprasadtelukutla@gmail.com>
2025-06-22 15:04:13 +0530

    wroking in request params,query

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const app = express()
 
+//user/:userId/:name/:password => req.params
+//user => in postman user?userId=101 
 
-app.get("/user", (req,res)=>{
+
+app.get("/user/:userId/:name/:password", (req,res)=>{
+    console.log(req.params)
     res.send({firstName:"siva", second:"telukutla", age:"25"})
 })
 
