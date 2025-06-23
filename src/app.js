1408c72876331fb1b2432ee724b5cf8e54df7e77
sivaprasad1358 <sivaprasadtelukutla@gmail.com>
2025-06-23 10:49:07 +0530

    middleware&error handling

--- src/app.js
+++ src/app.js
@@ -9,12 +9,38 @@ const app = express()
 //(req,res,next) => it is a route handler 
 //if responsd is empty.the response state is pending 
 
-app.get("/user", [(req,res,next)=>{
-    // res.send("user data 1") => if first response is empty its go to second response
-    next()
-},(req,res, next)=>{
-    res.send("user data 2") //in the last response don't use next() function its give error
-}])
+// app.get("/user", [(req,res,next)=>{
+//     // res.send("user data 1") => if first response is empty its go to second response
+//     next()
+// },(req,res, next)=>{
+//     res.send("user data 2") //in the last response don't use next() function its give error
+// }])
+
+app.use("/", (req,res, next)=>{
+    const auth = "xyz";
+    if(auth !== "xyz"){
+        res.send("user is not auth")
+    }
+    else{
+        next()
+    }
+})
+
+app.get("/user", (req,res)=>{
+    res.send("user data")
+})
+
+app.post("/user/postdata", (req,res)=>{
+    res.send("user post is kk")
+})
+
+app.put("/user/putdata", (req,res)=>{
+    res.send("the data is updated")
+})
+
+app.delete("/user/deletedata", (req,res)=>{
+    res.send("user is delete")
+})
 
 app.listen(3000, ()=>{
     console.log("server is running")
