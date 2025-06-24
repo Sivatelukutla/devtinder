6a63879b79823334be90aa3eecb98b0498f3723f
sivaprasad1358 <sivaprasadtelukutla@gmail.com>
2025-06-24 16:08:51 +0530

    the data is send to database by usong post

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-    const express = require("express")
+const express = require("express")
 const connectDB = require("./config/database")
 const user = require("./models/user.js")
 const app = express()
@@ -6,87 +6,16 @@ const app = express()
 app.use(express.json())
 
 app.post("/signup", async(req,res)=>{
-    // console.log(req.body)
-    const users = new user(req.body)
+    const users = req.body 
     try{
-        await users.save()
-        res.send("user is created successfully")
-    }
-    catch{
-        res.status(400).send("error")
-    }
-})
-
-app.get("/user", async(req,res)=>{
-    const userEmailId = req.body.emailId 
-    try{
-        const userD = await user.find({emailId:userEmailId})
-        if(userD.length === 0){
-            res.status(400).send("user is not found")
-        }
-        else{
-            res.send(userD)
-        }
-    }catch(err){
-        res.status(400).send("error", err)
-    }
-})
-
-
-//feed api =>gt all users data 
-
-app.get("/feed", async(req,res)=>{
-    
-    try{
-        const dataUser = await user.find({})
-        if(dataUser.length === 0){
-            res.status(400).send("data is not found")
-        }
-        else{
-            res.send(dataUser)
-        }
+        const userData = new user(users)
+        userData.save()
+        res.send(userData)
     }
     catch(err){
-        res.send("error", err)
+        res.send(err)
     }
 })
-
-//findeOne method 
-
-app.get("/users", async(req,res)=>{
-    const userEmailId = req.body.emailId 
-    try{
-        const userD = await user.findOne({emailId:userEmailId})
-        if(userD.length === 0){
-            res.status(400).send("user is not found")
-        }
-        else{
-            res.send(userD)
-        }
-    }catch(err){
-        res.status(400).send("error", err)
-    }
-})
-
-app.patch("/patch", async (req, res) => {
-  const { emailId} = req.body.emailId;
-  const body = req.body
-
-  try {
-    const updatedUser = await user.findOneAndUpdate(
-      emailId, body          // Return the updated document
-    );
-
-    if (!updatedUser) {
-      return res.status(404).send("User not found");
-    }
-
-    res.send(updatedUser);
-  } catch (err) {
-    res.status(400).send("Error: " + err.message);
-  }
-});
-
 
 
 connectDB().then(()=>{
